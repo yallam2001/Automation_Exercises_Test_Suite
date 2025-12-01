b364f1b84ca1b9eb59ff34e69a45204cab6c53f1
Yahia Allam <yahiallam@gmail.com>
2025-12-01 23:41:01 +0200

    Misc changes

--- src/main/java/mySeleniumFramework/self_selenium.java
+++ src/main/java/mySeleniumFramework/self_selenium.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
